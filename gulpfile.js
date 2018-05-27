6d458a2b7fe7008f2174df74aedd418623f548ed
Taigah <taiah.dev@gmail.com>
2018-05-27 20:33:50 +0200

    adding default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
-'use strict';
- 
 const gulp = require('gulp')
 const sass = require('gulp-sass')
+const runSequence = require('run-sequence')
 
 gulp.task('sass', function () {
   return gulp.src('./public/stylesheets/style.sass')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./public/stylesheets'))
-});
+})
  
 gulp.task('sass:watch', function () {
   gulp.watch('./public/stylesheets/**/*.sass', ['sass'])
-});
+})
+
+gulp.task('default', done => {
+  runSequence('sass', done)
+})
